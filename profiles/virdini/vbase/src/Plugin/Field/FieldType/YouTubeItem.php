49e9a83b4dca121bd08f4ad1d138a98dce93656c
Alexander Tsybenko <alexander@tsybenko.info>
2021-04-01 13:02:02 +0300

    Removed YouTubeItem unused classes

--- profiles/virdini/vbase/src/Plugin/Field/FieldType/YouTubeItem.php
+++ profiles/virdini/vbase/src/Plugin/Field/FieldType/YouTubeItem.php
@@ -2,9 +2,7 @@
 
 namespace Drupal\vbase\Plugin\Field\FieldType;
 
-use Drupal\Core\Field\FieldItemInterface;
 use Drupal\Core\Field\FieldItemBase;
-use Drupal\Core\Field\FieldDefinitionInterface;
 use Drupal\Core\Field\FieldStorageDefinitionInterface;
 use Drupal\Core\TypedData\DataDefinition;
 
