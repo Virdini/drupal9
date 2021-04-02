fec753904b6362d0e47c5e90ed871c37476db4a1
Alexander Tsybenko <alexander@tsybenko.info>
2021-04-02 12:28:12 +0300

    Fixed YouTubeWidget form element

--- profiles/virdini/vbase/src/Plugin/Field/FieldWidget/YouTubeWidget.php
+++ profiles/virdini/vbase/src/Plugin/Field/FieldWidget/YouTubeWidget.php
@@ -24,28 +24,28 @@ class YouTubeWidget extends WidgetBase {
    */
   public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
     $properties = $items->getItemDefinition()->getPropertyDefinitions();
-    $element['value'] = $element + [
+    $element['#type'] = $element['#title_display'] == 'invisible' ? 'container' : 'fieldset';
+    $element['#attributes']['class'][] = 'vbase-field-widget-grid';
+    $element['value'] = [
       '#type' => 'textfield',
-      '#default_value' => isset($items[$delta]->value) ? $items[$delta]->value : NULL,
-      '#size' => 15,
+      '#title' => $properties['value']->getLabel(),
+      '#default_value' => $items[$delta]->value,
+      '#required' => $element['#required'],
     ];
-    $element['#type'] = 'fieldset';
-    $element['#attributes']['class'][] = 'vbase-youtube-container';
-    $element['value']['#title'] = $properties['value']->getLabel();
     $element['width'] = [
       '#type' => 'number',
       '#title' => $properties['width']->getLabel(),
-      '#default_value' => isset($items[$delta]->width) ? $items[$delta]->width : 0,
+      '#default_value' => $items[$delta]->width ?: 0,
     ];
     $element['height'] = [
       '#type' => 'number',
       '#title' => $properties['height']->getLabel(),
-      '#default_value' => isset($items[$delta]->height) ? $items[$delta]->height : 0,
+      '#default_value' => $items[$delta]->height ?: 0,
     ];
     $element['resp'] = [
       '#type' => 'checkbox',
       '#title' => $properties['resp']->getLabel(),
-      '#default_value' => isset($items[$delta]->resp) ? $items[$delta]->resp : 0,
+      '#default_value' => $items[$delta]->resp,
     ];
     return $element;
   }
